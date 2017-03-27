7e412679f607379c5e81a538828fc87592fe6caa
Nick DeMaster <ndemaster@demaster-mbpro.local>
2017-03-27 14:55:53 -0400

    Adding logging and silent mode

--- mutts.sh
+++ mutts.sh
@@ -1,4 +1,4 @@
-#1/bin/bash
+#!/bin/bash
 
 set -m # Enable Job Control
 
@@ -6,18 +6,23 @@ set -m # Enable Job Control
 usage() { 
 
 cat << EOF
-usage: $0 [-t <number>] [-r <number>] [-d <number>]
- This script is a wrapper for the mysql 
+usage: $0 [-t <number>] [-r <number>] [-d <number>] [-s] -j <jobfile>
+ This script is a wrapper for the multi-threaded tester (mutts).
+   
+ This will run a (series of) command(s) in the <jobfile> over a specified number of threads (-t), a set number of times (-r)
+ 
  OPTIONS:
     -h        Show this message
     -t        Number of threads of the test to run (default: 1)
     -r        Number of times to run the test (default: 1)
     -d        Initial thread spawn delay in seconds (default: 10)
     -j        Job file location
-#TODO:    -l        Write inside output to file location
+    -p        Pause for  
+    -s        Make output silent
+    -l        Write inside output to file location
+
 EOF
 
-echo "" 
 exit 1;
 
 }
@@ -29,59 +34,62 @@ re='^[0-9]+$'
 t=1
 r=1
 d=10
+s=false
+l=
 
 #get arguments and set variable
-while getopts ":r:t:d:j:l:" o; do
+while getopts ":r:t:d:j:l:s" o; do
     case "${o}" in
-        t)
-           t=${OPTARG}
-            ;;
-        r)
-           r=${OPTARG}
-            ;;
-        d)
-           d=${OPTARG}
-            ;;
-        j)
-           j=${OPTARG}
-            ;;
-        l)
-           l=${OPTARG}
-            ;;
-        *)
-            usage
-            ;;
+        t) t=${OPTARG} ;;
+        r) r=${OPTARG} ;;
+        d) d=${OPTARG} ;;
+        s) s=true ;;
+        j) j=${OPTARG} ;;
+        l) l=${OPTARG} ;;
+        *) usage ;;
     esac
 done
 shift $((OPTIND-1))
 
 #logic checking around variable
-if [ -z "${t}" ] || [ -z "${r}" ] || [ -z "${d}" ]; then
+if [[ -z "${t}" ]] || [[ -z "${r}" ]] || [[ -z "${d}" ]] ; then
     usage
 fi
+
+#echo "POPULATION PASSED"
 
 if ! [[ $t =~ $re ]]; then
     usage
 fi
 
+#echo "T is a number"
+
 if ! [[ $r =~ $re ]]; then
     usage
 fi
+
+#echo "R is a number"
 
 if ! [[ $d =~ $re ]]; then
     usage
 fi
 
+#echo "D is a number"
+
+
 if ! [[ -f $j ]]; then
     usage
 fi
+
 
 #feedback to use running
 echo "Threads     = $t"
 echo "Runs/Thread = $r"
 echo "Start Delay = $d"
 echo "Job File    = $j"
-# TODO: echo "Log file    = $l"
+echo "Output Mode = $s"
+echo "Logfile     = $l"
+
 
 #set nicer names
 threads=$t
@@ -96,8 +104,22 @@ masterInitiate=$((masterStart + $delay))
 
 #spawn worker threads
 for i in `seq $threads`; do # start in parallel
- 
-  ./mutts_worker.sh $masterInitiate $runs $jobfile $logfile &  
+  if [[ $s = true ]]; then
+    if [[ -n "$logfile" ]]; then
+      > $logfile
+      ./mutts_worker.sh $masterInitiate $runs $jobfile >> $logfile 2>&1 & 
+
+    else
+      ./mutts_worker.sh $masterInitiate $runs $jobfile > /dev/null &
+    fi 
+  else
+    if [[ -n "$logfile" ]]; then
+      > $logfile
+      ./mutts_worker.sh $masterInitiate $runs $jobfile | tee -a $logfile &
+    else
+      ./mutts_worker.sh $masterInitiate $runs $jobfile &
+    fi
+  fi
 
 done
 
